fb494cf33f78c5edac46f8e77aa55cbeb4f5f217
iso88591 <2658218247@qq.com>
2021-08-10 16:38:51 +0800

    扫描view 添加圆形边框

--- app/src/main/java/grg/learn/myhencoderplus/views/GlanceView.kt
+++ app/src/main/java/grg/learn/myhencoderplus/views/GlanceView.kt
@@ -7,9 +7,11 @@ import android.graphics.Color
 import android.graphics.Path
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
-import android.util.Log
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.withSave
+import androidx.core.view.setPadding
+import grg.learn.myhencoderplus.R
 
 class GlanceView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -28,34 +30,39 @@ class GlanceView @JvmOverloads constructor(
         }
 
     init {
-        val ani = ObjectAnimator.ofFloat(this,
+        val ani = ObjectAnimator.ofFloat(
+            this,
             "offset",
             0.2f,
-            0.75f)
+            0.75f
+        )
         ani.repeatCount = -1
         ani.duration = 800
         ani.startDelay = 200
         ani.start()
 
+
     }
 
     private val roundPath = Path()
 
+    private val ringDrawable = ContextCompat.getDrawable(context, R.drawable.ic_round_ring)!!
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        val radius = width/2f
-        roundPath.addCircle(width/2f,height/2f,radius,Path.Direction.CW)
+        val radius = width / 2f
+        roundPath.addCircle(width / 2f, height / 2f, radius, Path.Direction.CW)
     }
 
-    override fun dispatchDraw(canvas: Canvas){
+    override fun dispatchDraw(canvas: Canvas) {
         super.dispatchDraw(canvas)
         gradient.setBounds(
-            0,0,width,height
+            0, 0, width, height
         )
         canvas.withSave {
             clipPath(roundPath)
-            translate(0f,offset * height)
+            translate(0f, offset * height)
             clipRect(
                 0f,
                 0f,
@@ -65,6 +72,13 @@ class GlanceView @JvmOverloads constructor(
             gradient.draw(canvas)
 
         }
+
+        ringDrawable.setBounds(
+            0,0,
+            width,
+            height
+        )
+        ringDrawable.draw(canvas)
 
     }
 }
